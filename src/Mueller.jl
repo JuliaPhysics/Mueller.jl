0cb4ff11ca68c1c442ebc831e75e044262663cff
Miles Lucas <mdlucas@hawaii.edu>
2021-12-16 06:31:31 -0600

    add linear polarizers and tests

--- src/Mueller.jl
+++ src/Mueller.jl
@@ -1,5 +1,27 @@
 module Mueller
 
-# Write your package code here.
+using StaticArrays
 
+export rotation, linear_polarizer
+
+# Various formulae for polarization components
+function rotation(T, θ)
+    sin2t, cos2t = sincos(2 * θ)
+    return SA{T}[1 0 0 0
+              0 cos2t sin2t 0
+              0 -sin2t cos2t 0
+              0 0 0 1]
 end
+rotation(θ) = rotation(Float64, θ)
+
+function linear_polarizer(T::Type, gamma=0, p=1)
+    sin2g, cos2g = sincos(2 * gamma)
+    I = T(p^2 / 2)
+    return I * SA{T}[1 cos2g 0 0
+                 cos2g 1 0 0
+                 0 0 sin2g 0 
+                 0 0 0 sin2g]    
+end
+linear_polarizer(args...) = linear_polarizer(Float64, args...)
+
+end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,49 @@
 using Mueller
 using Test
+using Unitful: °
 
 @testset "Mueller.jl" begin
-    # Write your tests here.
+
+    @testset "linear polarizers" for T in [Float32, Float64, BigFloat]
+        @testset "horizontal" begin
+            horizontal = linear_polarizer(T)
+
+            # parallal light perfect
+            S = T[1, 1, 0, 0]
+            Sp = horizontal * S
+            @test Sp ≈ T[1, 1, 0, 0]
+
+            # orthogonal light blocked
+            S = T[1, -1, 0, 0]
+            Sp = horizontal * S
+            @test Sp ≈ zeros(T, 4)
+
+            # mixed light attenuated
+            S = T[1, 1/√2, 1/√2, 0]
+            Sp = horizontal * S
+            @test Sp ≈ 1/2 * T[1 + 1/√2, 1  + 1/√2, 0, 0]
+        end
+        @testset "vertical" begin
+            vertical = linear_polarizer(T, 90°)
+            @test vertical ≈ 0.5 * T[1 -1 0 0;
+                                     -1  1 0 0
+                                     0 0 0 0
+                                     0 0 0 0]
+
+            # parallal light perfect
+            S = T[1, -1, 0, 0]
+            Sp = vertical * S
+            @test Sp ≈ T[1, -1, 0, 0]
+
+            # orthogonal light blocked
+            S = T[1, 1, 0, 0]
+            Sp = vertical * S
+            @test Sp ≈ zeros(T, 4)
+
+            # mixed light attenuated
+            S = T[1, -1/√2, 1/√2, 0]
+            Sp = vertical * S
+            @test Sp ≈ 1/2 * T[1 + 1/√2, -1 - 1/√2, 0, 0]
+        end
+    end
 end
